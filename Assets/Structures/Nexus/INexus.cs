b0ef8855a3c4fc743e28503f4d6b5da4c29a7b09
Justin Koh <jkoh8@uwo.ca>
2020-02-24 23:59:57 -0500

    added interface and monobehavior class for nexus

---
+++ Assets/Structures/Nexus/INexus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface INexus
+{
+    void SpawnMinion();
+
+    float GetNexusHealth();
+
+    List<GameObject> GetNexusMinions();
+
+    string GetNexusTeam();
+}
---
+++ Assets/Structures/Nexus/Nexus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nexus : MonoBehaviour, INexus
+{
+    [Header("Minion Prefab")]
+    [SerializeField] GameObject _minion_prefab; // reference to prefab object
+
+    [Space]
+    [Header("Team Settings")]
+    [SerializeField] string _team; // which team the nexus belongs to (replace type with custom class/enum)
+
+    private float _nexus_health;
+    private List<GameObject> _minions; // list of minions that belong to the nexus
+
+    void Start()
+    {
+        _minions = new List<GameObject>();    
+    }
+
+    public void SpawnMinion()
+    {
+        // instantiate a minion prefab at the world space origin
+        _minions.Add(Instantiate(_minion_prefab, Vector3.zero, Quaternion.identity));
+    }
+
+    public float GetNexusHealth()
+    {
+        return _nexus_health;
+    }
+
+    public List<GameObject> GetNexusMinions()
+    {
+        return _minions;
+    }
+
+    public string GetNexusTeam()
+    {
+        return _team;
+    }
+}
