77cd1ea38d6c1634f8a3ef8558b5cac3f81bd6ea
seanblee <15sl18@queensu.ca>
2020-03-06 02:22:50 -0500

    rodenteffects

---
+++ Assets/Game/Shared/Effects/RodentEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RodentEffect : MonoBehaviour
+{
+    public Rodent rodent;
+
+    public float duration;
+
+    public float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Effect", startTime);
+        Invoke("StopEffect", duration);
+    }
+
+    protected virtual void Effect() { }
+
+    protected virtual void StopEffect()
+    {
+        CancelInvoke();
+        Destroy(gameObject);
+    }
+        
+   
+}
